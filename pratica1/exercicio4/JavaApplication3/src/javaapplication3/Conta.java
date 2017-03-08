03018531dbe7e87cc64a54341ead6c4ca53bfa76
carolinemontone <carolinesmontone@gmail.com>
2017-03-08 11:18:18 -0300

    java

---
+++ pratica1/exercicio4/JavaApplication3/src/javaapplication3/Conta.java
@@ -0,0 +1,38 @@
+
+package javaapplication3;
+
+public class Conta {
+    int numero;
+    String titular;
+    String cpf;
+    double saldo;
+    
+    void visualizarSaldo() {
+        System.out.println("Saldo =" + this.saldo);
+        
+    }
+    
+    void depositar(double valor) {
+       if (valor > 0) {
+           this.saldo = this.saldo + valor; 
+       }
+    }
+    
+    void sacar (double value){
+        
+        if (value >= this.saldo) {
+            
+            this.saldo = this.saldo - value;
+            
+        } else {
+            
+            System.out.println("Quer um empréstimo?");
+            
+        }
+        
+    }
+    
+    void transferirDinheiro(){
+        
+    }
+}
